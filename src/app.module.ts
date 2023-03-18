45c4358d2bde70ff5b5f4c91e68430b11930ccc1
Dmitrii <da.mingalev@gmail.com>
2023-03-18 18:09:53 +0400

    Add CI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 30,
+    }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        ENVIRONMENT: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,48 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+    cors: {
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    },
+  });
+
+  const configService = app.get(ConfigService);
+  const environment = configService.get<string>('ENVIRONMENT');
+  const version = '0.0.1';
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: false,
+      }),
+  );
+  app.use(helmet());
+  app.use(cookieParser());
+
+  if (['local-dev', 'development'].includes(environment)) {
+    const options = new DocumentBuilder()
+        .setTitle('BonBonus – backend service')
+        .setDescription('Description of API methods.')
+        .addBearerAuth()
+        .setVersion(version)
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('swagger', app, document);
+    Logger.log('Swagger has been launched', 'Swagger');
+  }
+
+  const port = configService.get<number>('PORT');
+  await app.listen(port);
 }
 bootstrap();
